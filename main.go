8214998a9e6d5ec434c7661b955edea98cd9788b
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-06-17 23:22:15 +0600

    Max module

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/GenesisBlock3301/GoPackages/src/max"
 )
 
 func main() {
-	max.Hello()
+	fmt.Println(max.Max([]int{1, 2, 3, 4, 5}))
+	fmt.Println(max.Max([]string{"a", "b", "c"}))
 }
--- src/max/max.go
+++ src/max/max.go
@@ -1,7 +1,30 @@
 package max
 
-import "log"
+import (
+	"reflect"
+)
 
-func Hello() {
-	log.Print("Hello sifat")
+func Max(values interface{}) interface{} {
+	var _max int
+	if reflect.TypeOf(values).Kind() == reflect.Slice && reflect.TypeOf(values).Elem().Kind() == reflect.Int {
+		slice := reflect.ValueOf(values)
+		_max = slice.Index(0).Interface().(int)
+		for i := 1; i < slice.Len(); i++ {
+			value := slice.Index(i).Interface().(int)
+			if _max < value {
+				_max = value
+			}
+		}
+	} else if reflect.TypeOf(values).Kind() == reflect.Slice && reflect.TypeOf(values).Elem().Kind() == reflect.String {
+		slice := reflect.ValueOf(values)
+		sMax := slice.Index(0).Interface().(string)
+		for i := 1; i < slice.Len(); i++ {
+			value := slice.Index(i).Interface().(string)
+			if sMax < value {
+				sMax = value
+			}
+		}
+		return sMax
+	}
+	return _max
 }
